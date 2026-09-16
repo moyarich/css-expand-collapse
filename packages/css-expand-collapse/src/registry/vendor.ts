9ae97fd40393fccc45a2f45902933b3353cace96
moyarich <moyarich@hotmail.com>
2026-09-16 12:23:25 -0500

    feat: support vendor shorthand strategies

--- packages/css-expand-collapse/src/registry/vendor.ts
+++ packages/css-expand-collapse/src/registry/vendor.ts
@@ -1,17 +1,20 @@
-import { unsupported } from "./helpers.js";
+import { components, cssom } from "./helpers.js";
 import type { ShorthandDefinitionMap } from "./types.js";
 
 export const VENDOR_SHORTHANDS: ShorthandDefinitionMap = {
-  "-webkit-text-stroke": unsupported([
-    "-webkit-text-stroke-width",
-    "-webkit-text-stroke-color",
-  ]),
-  "-webkit-border-before": unsupported([
-    "-webkit-border-before-width",
-    "-webkit-border-before-style",
-    "-webkit-border-before-color",
-  ]),
-  "-webkit-mask-box-image": unsupported([
+  "-webkit-text-stroke": components(
+    ["-webkit-text-stroke-width", "-webkit-text-stroke-color"],
+    ["0", "currentcolor"],
+  ),
+  "-webkit-border-before": {
+    longhands: [
+      "-webkit-border-before-width",
+      "-webkit-border-before-style",
+      "-webkit-border-before-color",
+    ],
+    strategy: "triple",
+  },
+  "-webkit-mask-box-image": cssom([
     "-webkit-mask-box-image-source",
     "-webkit-mask-box-image-slice",
     "-webkit-mask-box-image-width",
