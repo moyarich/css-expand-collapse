862a59b6050b367ba6c8a1eed709f538b8e29f05
moyarich <moyarich@hotmail.com>
2026-09-16 21:10:41 -0500

    refactor: add examples barrel

---
+++ apps/playground/src/examples/index.ts
@@ -0,0 +1,13 @@
+export {
+  COMPUTED_EXPORT_EXAMPLE,
+  COMPUTED_EXPORT_EXAMPLE_ID,
+} from "./computedExport.js";
+
+export {
+  EXAMPLE_GROUPS,
+  SHORTHAND_EXAMPLES,
+  getShorthandExample,
+  type ExampleGroup,
+  type ExampleProperty,
+  type ShorthandExample,
+} from "./shorthands.js";
