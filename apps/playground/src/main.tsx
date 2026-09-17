c958255f4e27c518ef8f5e6e717f09a5e74daf91
moyarich <moyarich@hotmail.com>
2026-09-17 09:42:54 -0500

    feat: load API playground styles

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./monaco";
 import { App } from "./App";
 import "./styles.css";
+import "./functionPlayground/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
