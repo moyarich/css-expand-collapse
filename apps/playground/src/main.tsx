501264418ae447602049532f612df0609a8fca75
moyarich <moyarich@hotmail.com>
2026-09-18 03:55:53 -0500

    feat(playground): add React Router v8

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router";
 import "./components/MonacoEditor/setup";
 import { App } from "./App";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
